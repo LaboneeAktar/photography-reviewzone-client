09122a32d7cb152a85af376907060653827bc529
laboneeaktar <labonipub@gmail.com>
2022-11-08 11:51:40 +0600

    Added a top banner

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,41 @@
 import React from "react";
+import bannerImg from "../../assets/images/banner.gif";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home.</h1>
+      <section>
+        <div className="dark:bg-violet-400 bg-teal-300">
+          <div className="container flex flex-col items-center px-4 py-10 pb-24 mx-auto text-center lg:pb-56 md:py-16 md:px-10 lg:px-32 dark:text-gray-900">
+            <h1 className="text-xl font-bold leading-none lg:text-3xl dark:text-gray-900">
+              Get All Services & Give Your Review Here!!
+            </h1>
+            <p className="mt-6 mb-5 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">
+              Enjoy your favourite type of pictures and give more reviews to
+              encourage. Your review can give a bit of clarity to make a
+              decision. Do a favour by writing a few words about your
+              experience...
+            </p>
+            <div className="flex justify-between">
+              <button
+                type="button"
+                className="flex justify-between items-center px-4 py-3 m-2 text-lg border border-gray-800 rounded bg-gray-800 text-white dark:border-gray-700 dark:text-gray-900 hover:bg-teal-700 hover:border-teal-500 "
+              >
+                <p>Let's Start</p>
+                <p className="pl-3">
+                  <FaArrowRight />
+                </p>
+              </button>
+            </div>
+          </div>
+        </div>
+        <img
+          src={bannerImg}
+          alt=""
+          className="w-4/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 dark:bg-gray-500"
+        />
+      </section>
     </div>
   );
 };
