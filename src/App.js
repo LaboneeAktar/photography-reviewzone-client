c96e29282375305d80f279426d2bca62662f7923
laboneeaktar <labonipub@gmail.com>
2022-11-08 09:55:47 +0600

    set title icon and add font style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import routes from "./Router/Router/Router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <RouterProvider router={routes}></RouterProvider>
+      <Toaster></Toaster>
+    </div>
+  );
 }
 
 export default App;
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVmJxq-okwEUbGxx-7R137O360t_T7cZs",
-  authDomain: "photography-reviewzone.firebaseapp.com",
-  projectId: "photography-reviewzone",
-  storageBucket: "photography-reviewzone.appspot.com",
-  messagingSenderId: "311192781446",
-  appId: "1:311192781446:web:685a4286207949ca640707",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
