8ba98cfed61910641eb105a85e8b078b2579c0fd
laboneeaktar <labonipub@gmail.com>
2022-11-09 22:05:03 +0600

    update README.md file

--- src/Pages/ServiceDetails/ReviewForm.js
+++ src/Pages/ServiceDetails/ReviewForm.js
@@ -4,100 +4,125 @@ import { AuthContext } from "../../contexts/AuthProvider";
 const ReviewForm = ({ service }) => {
   const { user } = useContext(AuthContext);
   const { _id, title } = service;
-  console.log(service);
+  //   console.log(service);
 
-  const handleSubmitReview = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const ratings = form.ratings.value;
+    const message = form.review.value;
+
+    // console.log(name, email, ratings, message);
+
+    const review = {
+      serviceId: _id,
+      serviceName: title,
+      reviewerName: name,
+      email,
+      ratings,
+      message,
+    };
+
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.error(error));
   };
+
   return (
-    <div>
-      <form onSubmit={handleSubmitReview}>
-        <h2 className="text-2xl text-center py-5">
-          Add Your Review of <strong> "{title}" </strong>
-        </h2>
-
-        <div className="bg-teal-300 p-10 lg:mx-16 mx-5 rounded-md mb-16">
-          <fieldset className="lg:ml-[360px] my-5 space-y-1 dark:text-gray-100">
-            <div className="flex">
-              <span className="flex items-center px-3 pointer-events-none text-lg rounded-l-md bg-gray-300">
-                Service Id
-              </span>
-              <input
-                name="serviceid"
-                type="text"
-                placeholder=""
-                defaultValue={_id}
-                readOnly
-                className="lg:w-96 w-full px-4 py-4 rounded-r-md border-white bg-slate-100 text-black  focus:border-violet-400 font-normal text-[16px]"
-                required
-              />
-            </div>
-          </fieldset>
-
-          <div className="lg:flex lg:mx-auto">
-            <fieldset className="lg:ml-24 space-y-1 dark:text-gray-100 mb-5 lg:mb-0">
-              <div className="flex mx-auto">
-                <span className="flex items-center px-3 pointer-events-none text-lg rounded-l-md bg-gray-300">
+    <div className="lg:mb-16 mb-5">
+      <h2 className="text-center text-2xl">
+        Add Your Review of <strong>"{title}"</strong>
+      </h2>
+      <div className="py-10 px-5 lg:pb-1">
+        <div className="w-full max-w-3xl p-8 rounded-xl bg-teal-300 text-black mx-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="space-y-6 ng-untouched ng-pristine ng-valid"
+          >
+            <div className="lg:flex lg:justify-between w-full lg:pt-5">
+              <div className="space-y-1 text-sm">
+                <label
+                  htmlFor="name"
+                  className="block text-black text-center text-lg"
+                >
                   Name
-                </span>
+                </label>
                 <input
                   type="text"
                   name="name"
                   id="name"
-                  placeholder="Enter Name"
                   defaultValue={user?.displayName}
-                  className="lg:w-96 w-full px-4 py-4 rounded-r-md border-white bg-slate-100 text-black  focus:border-violet-400 font-normal text-[16px] lg:mr-10"
+                  placeholder="Enter Your Name"
+                  className="lg:w-80 w-full px-4 py-4 rounded-md border-white bg-slate-100 text-black font-normal text-[16px] focus:border-violet-400"
                   required
                 />
               </div>
-            </fieldset>
-            <fieldset className="space-y-1 dark:text-gray-100">
-              <div className="flex">
-                <span className="flex items-center px-3 pointer-events-none text-lg rounded-l-md bg-gray-300">
+
+              <div className="space-y-1 text-sm">
+                <label
+                  htmlFor="email"
+                  className="block text-black text-center text-lg"
+                >
                   Email
-                </span>
+                </label>
                 <input
-                  name="email"
                   type="email"
-                  placeholder="Email Address"
+                  name="email"
+                  id="email"
                   defaultValue={user?.email}
                   readOnly
-                  className="lg:w-96 w-full px-4 py-4 rounded-r-md border-white bg-slate-100 text-black  focus:border-violet-400 font-normal text-[16px]"
+                  placeholder="Enter Your Email"
+                  className="lg:w-80 w-full px-4 py-4 rounded-md border-white bg-slate-100 text-black  focus:border-violet-400 font-normal text-[16px]"
                   required
                 />
               </div>
-            </fieldset>
-          </div>
-          <div className="mx-auto">
-            <textarea
-              id="review"
-              placeholder="Write a Review"
-              className="lg:w-[500px] w-full p-5 h-40 rounded-md dark:border-gray-700 dark:text-gray-900 mt-5 lg:mx-[350px]"
-              required
-            ></textarea>
-          </div>
-          <fieldset className="lg:ml-[360px] mt-5 space-y-1 dark:text-gray-100">
-            <div className="flex">
-              <span className="flex items-center px-3 pointer-events-none text-lg rounded-l-md bg-gray-300">
+            </div>
+
+            <div className="space-y-1 text-sm text-center">
+              <label htmlFor="ratings" className="block text-black text-lg">
                 Ratings
-              </span>
+              </label>
               <input
+                type="ratings"
                 name="ratings"
-                type="number"
+                id="ratings"
                 placeholder="Enter Ratings"
-                className="lg:w-96 w-full px-4 py-4 rounded-r-md border-white bg-slate-100 text-black  focus:border-violet-400 font-normal text-[16px]"
+                className="lg:w-80 w-full px-4 py-4 rounded-md border-white bg-slate-100 text-black focus:border-violet-400 font-normal text-[16px] text-center"
+                required
               />
             </div>
-          </fieldset>
+            <div className="space-y-1 text-sm">
+              <label
+                htmlFor="review"
+                className="block text-black text-lg"
+              ></label>
+              <textarea
+                type="text"
+                name="review"
+                id="review"
+                placeholder="Write a Review"
+                className="w-full h-40 rounded-md p-5 border-white bg-slate-100 text-black focus:border-violet-400 font-normal text-[16px]"
+                required
+              />
+            </div>
 
-          <button
-            type="button"
-            className="px-7 py-2 mt-5 text-[18px] font-normal border-2 hover:border-teal-500 rounded hover:text-white hover:bg-teal-600 border-teal-500 text-black dark:border-gray-100  dark:text-gray-100 lg:ml-[550px]"
-          >
-            Send
-          </button>
+            <button className="lg:ml-[300px] block px-6 py-2 text-lg font-normal border border-black rounded text-black hover:bg-teal-500 hover:border-teal-500 dark:border-gray-100  dark:text-gray-100">
+              Send
+            </button>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
